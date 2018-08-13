f214bc7240f8c74a4bacbf0963f48c49f9ade4df
stock1218 <johnsamuels1218@gmail.com>
2018-08-13 05:51:17 +0100

    Add modules and files

--- app.js
+++ app.js
@@ -3,9 +3,12 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var bodyParser = require('body-parser');
+var request = require('request');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var webhookRouter = require('./routes/webhook');
 
 var app = express();
 
@@ -14,6 +17,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.use(logger('dev'));
+app.use(bodyParser.json());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -21,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/webhook', webhookRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
